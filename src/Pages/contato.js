5f4a33db11c39b6e546d36278cd74dfd2d83b67e
Mateus <mateusmelosf@gmail.com>
2022-02-09 18:17:08 -0300

    aula 1 completa

--- src/Pages/contato.js
+++ src/Pages/contato.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import { Grid, Button, TextField } from '@material-ui/core/';
 
 const Contatos = () => {
@@ -8,9 +8,16 @@ const Contatos = () => {
                 <TextField id="name" label="Name" fullWidth/>
                 <TextField id="message" label="Message" fullWidth/>
             </Grid>
-            <Button variant="contained" color="primary">
+            <Button className="mt-2" variant="contained" color="primary">
                 Sent
             </Button>
+            <div className="card mt-2">
+                <div className="card-body">
+                    <h5 className="card-title">Author</h5>
+                    <p className="card-text">message content.</p>
+                    <p className="card-text"><small class="text-muted">message date</small></p>
+                </div>                
+            </div>            
         </>
     )
 }
